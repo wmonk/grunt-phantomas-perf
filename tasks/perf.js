639bf57fafabe73527d6106082c83e3e9dee47b0
Will Monk <will@williammonk.co.uk>
2015-02-10 14:28:40 +0000

    Allow trials to be specified

--- tasks/perf.js
+++ tasks/perf.js
@@ -11,46 +11,96 @@ var errors = {
     255: 'Phantomas has failed with an unknown error.'
 };
 
-function testPage (args) {
-    var url = args[0], assertions = args[1].assertions, options = args[1].phantomasOptions, grunt = args[2];
+function olympic(values) {
+    var results = values;
+    var max, min;
 
-    return new Promise(function (res, rej) {
+    // Find and remove the maximum.
+    max = Math.max.apply(null, results);
+    results.splice(results.indexOf(max), 1);
+
+    // Find and remove the minimum.
+    min = Math.min.apply(null, results);
+    results.splice(results.indexOf(min), 1);
+
+    return results;
+}
+
+function createAverage(aggregate) {
+    var vals = (aggregate.length) || 1;
+    var average = {};
+
+    Object.keys(aggregate[0]).forEach(function (metric) {
+        average[metric] = [];
+
+        for (var i = 0; i < vals; i++) {
+            average[metric].push(aggregate[i][metric]);
+        }
+
+        average[metric] = olympic(average[metric]);
+
+        average[metric] = average[metric].reduce(function (total, num) {
+            return total + num;
+        });
+
+        average[metric] = Math.round(average[metric] / (vals - 2));
+    });
+
+    return average;
+}
+
+function testPage(args) {
+    var url = args[0],
+        assertions = args[1].assertions,
+        trials = args[1].trials,
+        options = args[1].phantomasOptions,
+        grunt = args[2];
+
+    return new Promise(function (res) {
         console.log('');
         grunt.log.writeln('Analysing ' + chalk.bold(url));
-        console.log('');
 
         var assertFailed = false;
+        var trial = 0;
 
-        phantomas(url, options, function (err, json) {
-            if (err && err !== 252) {
-                grunt.fail.fatal(errors[err] || errors[255]);
-                return rej(err);
-            }
+        Promise.map(_.range(trials), function () {
+            return new Promise(function (res, rej) {
+                trial++;
+                phantomas(url, options, function (err, json) {
+                    if (err && err !== 252) {
+                        grunt.fail.fatal(errors[err] || errors[255]);
+                        return rej(err);
+                    }
 
-            var metrics = json.metrics;
+                    if (err === 252) {
+                        grunt.log.error('Your page did not finish doing things in less than ' + options.timeout + ' seconds');
+                    }
 
-            Object.keys(assertions).forEach(function (assertion) {
-                if (metrics[assertion] > assertions[assertion]) {
-                    assertFailed = true;
-                    grunt.log.error(assertion + ' failed');
-                    console.log('  ', chalk.gray('Wanted:'), assertions[assertion], chalk.gray('Got:'), metrics[assertion]);
-                } else {
-                    grunt.log.ok(assertion + ' passed');
-                    console.log('  ', chalk.gray('Wanted:'), assertions[assertion], chalk.gray('Got:'), metrics[assertion]);
-                }
+                    res(json.metrics);
+                }).progress(function (progress) {
+                    var percent = Math.floor(((progress / trials) * 100) + ((100 / trials) * (trial - 1)));
+                    process.stdout.clearLine();
+                    process.stdout.cursorTo(0);
+                    process.stdout.write('Loading page: ' + percent + '%');
+                });
             });
+        }, { concurrency: 1 })
+            .then(createAverage)
+            .then(function (metrics) {
+                console.log('\n');
+                Object.keys(assertions).forEach(function (assertion) {
+                    if (metrics[assertion] > assertions[assertion]) {
+                        assertFailed = true;
+                        grunt.log.error(assertion + ' failed');
+                        console.log('  ', chalk.gray('Wanted:'), assertions[assertion], chalk.gray('Got:'), metrics[assertion]);
+                    } else {
+                        grunt.log.ok(assertion + ' passed');
+                        console.log('  ', chalk.gray('Wanted:'), assertions[assertion], chalk.gray('Got:'), metrics[assertion]);
+                    }
+                });
 
-            res(!assertFailed);
-        })
-        .progress(function (progress) {
-            process.stdout.clearLine();
-            process.stdout.cursorTo(0);
-            process.stdout.write('Loading page: ' + (progress * 100) + '%');
-
-            if (progress === 1) {
-                process.stdout.write('\n\n');
-            }
-        });
+                res(!assertFailed);
+            });
     });
 }
 
@@ -59,8 +109,13 @@ module.exports = function (grunt) {
         var done = this.async();
         var options = _.assign({
             urls: [],
-            phantomasOptions: {}
+            phantomasOptions: {
+                timeout: 10
+            },
+            trials: 1
         }, this.options());
+
+        options.trials = options.trials + 2;
 
         if (typeof options.url === 'string') {
             options.urls.push(options.url);
@@ -81,13 +136,17 @@ module.exports = function (grunt) {
             return [url, options, grunt];
         });
 
-        Promise.map(generated, testPage, { concurrency: 1 })
-            .then(function (data) {
-                if (data.length !== 0 && data.filter(function (x) { return x; }).length !== data.length) {
-                    return grunt.fail.fatal('Some assertions did not pass');
-                }
+        Promise.map(generated, testPage, {
+            concurrency: 1
+        })
+        .then(function (data) {
+            if (data.length !== 0 && data.filter(function (x) {
+                return x;
+            }).length !== data.length) {
+                return grunt.fail.fatal('Some assertions did not pass');
+            }
 
-                done();
-            });
+            done();
+        });
     });
 };
